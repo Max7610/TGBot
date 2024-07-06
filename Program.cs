348b75dfbc1e54df368f8e1ff4463e837099431d
Max7610 <56383853+Max7610@users.noreply.github.com>
2024-07-06 17:15:49 +0300

    3

--- Program.cs
+++ Program.cs
@@ -3,9 +3,8 @@ using System.Globalization;
 using TGBot.Classes.telegramBot;
 
 Console.WriteLine("Запуск программ");
-//TelegramBot telegramBot = new TelegramBot();
-var os = Environment.OSVersion;
-Console.WriteLine(os.Platform);
+TelegramBot telegramBot = new TelegramBot();
+
 
 Console.ReadKey();
 
