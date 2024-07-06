bf109833dd6dc90f869fd29e97177b64005b2c53
Max7610 <56383853+Max7610@users.noreply.github.com>
2024-07-06 18:03:03 +0300

    13

--- Program.cs
+++ Program.cs
@@ -5,5 +5,9 @@ using TGBot.Classes.telegramBot;
 Console.WriteLine("Запуск программ");
 
 TelegramBot telegramBot = new TelegramBot();
+while (true)
+{
+    Thread.Sleep(500000);
+}
 Console.WriteLine("END");
 
