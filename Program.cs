2534626ae38f260d5f9802c0e82ba4962c52fa40
Max7610 <56383853+Max7610@users.noreply.github.com>
2024-07-06 17:49:31 +0300

    10

--- Program.cs
+++ Program.cs
@@ -3,23 +3,7 @@ using System.Globalization;
 using TGBot.Classes.telegramBot;
 
 Console.WriteLine("Запуск программ");
-A();
+
 TelegramBot telegramBot = new TelegramBot();
-
-
-
-
-void A()
-{
-    var d = Directory.GetFiles(Directory.GetCurrentDirectory());
-    foreach(var i in d)
-    {
-        Console.WriteLine(i);
-    }
-    var b = Directory.GetDirectories(Directory.GetCurrentDirectory());
-    foreach(var i in b)
-    {
-        Console.WriteLine(i);
-    }
-}
+Console.WriteLine("END");
 
