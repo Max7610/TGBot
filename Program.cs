97c067e9eb808b82b7bede69147100a5743bfdfe
Max7610 <56383853+Max7610@users.noreply.github.com>
2024-07-07 18:59:42 +0300

    36

--- Program.cs
+++ Program.cs
@@ -2,6 +2,11 @@
 using System.Globalization;
 using TGBot.Classes.telegramBot;
 
+string logFilePath = "path_to_your_log_file.log";
+
+// Очистка файла лога
+File.WriteAllText(logFilePath, string.Empty);
+
 
 Console.WriteLine("Запуск программ" + DateTime.Now);
 Start();
