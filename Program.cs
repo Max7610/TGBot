672f28e784760e3eae6a17f9552357f6cf44ac5f
Max7610 <56383853+Max7610@users.noreply.github.com>
2024-07-07 19:03:32 +0300

    38

--- Program.cs
+++ Program.cs
@@ -5,15 +5,7 @@ using TGBot.Classes.telegramBot;
 
 Console.WriteLine("Запуск программ" + DateTime.Now);
 Start();
-if (Console.IsInputRedirected)
-{
-    Console.WriteLine("Console input is redirected. Please provide input through arguments or other means.");
-}
-else
-{
-    Console.WriteLine("Press any key to continue...");
-    Console.ReadKey();
-}
+Console.Read();
 Console.WriteLine("END");
 
 async void Start()
